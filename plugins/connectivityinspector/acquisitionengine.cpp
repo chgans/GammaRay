90afc7e84ee58d85c53571097586c5f1021991e8
Christian Gagneraud <christian.gagneraud@navico.com>
2019-06-10 18:03:58 +1200

    Fix filter logic

--- plugins/connectivityinspector/acquisitionengine.cpp
+++ plugins/connectivityinspector/acquisitionengine.cpp
@@ -206,9 +206,9 @@ void AcquisitionEngine::registerObjectDiscriminator()
 {
     m_objectDiscriminator = new ObjectDiscriminator(objectFilterName(), this);
     m_objectDiscriminator->setDiscriminationRole(ObjectModel::ObjectIdRole);
-    auto input = new SingleColumnObjectProxyModel(this);
-    input->setSourceModel(m_probe->objectTreeModel());
-    m_objectDiscriminator->setSourceModel(input);
+    // auto input = new SingleColumnObjectProxyModel(this);
+    // input->setSourceModel(m_probe->objectTreeModel());
+    m_objectDiscriminator->setSourceModel(m_probe->objectTreeModel() /*input*/);
     m_objectDiscriminator->initialise(m_probe);
 }
 
